fb3c09fce8cd2c78356d6f49c11508502ab3611d
kai815 <h.kaizuka815@gmail.com>
2021-08-05 12:50:11 +0900

    payloadを試す

--- normal-nuxt/nuxt.config.js
+++ normal-nuxt/nuxt.config.js
@@ -26,6 +26,13 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+  generate: {
+    routes() {
+      return [
+        {route:'/post/test',payload:{test:"test"}}
+      ]
+    }
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ normal-nuxt/pages/post/_id.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  {{payload}}
+  <Tutorial/>
+</div>
+</template>
+
+<script>
+export default {
+  async asyncData({payload}){
+    console.log(payload);
+    return { payload:payload}
+  },
+  mounted(){
+    console.log(this.payload)
+  }
+}
+</script>
